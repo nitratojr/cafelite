cab6b1b315b71c52eb8c0d093b8b22e3e229d8c3
nitratojr <162368996+nitratojr@users.noreply.github.com>
2025-07-14 16:42:27 -0300

    Update server.ts

--- cafe/src/server.ts
+++ cafe/src/server.ts
@@ -30,7 +30,7 @@ AppDataSource.initialize()
     
     app.listen(PORT, '0.0.0.0', () => {
       console.log(`Servidor rodando na porta ${PORT}`);
-      // console.log(`Acesse: http://localhost:${PORT}`);
+      console.log(`Acesse: http://localhost:${PORT}`);
     });
   })
   .catch((error) => {
