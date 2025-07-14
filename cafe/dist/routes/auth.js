4121d79827e52c3609cfbd9661f4a0b44ed16c68
nitratojr <162368996+nitratojr@users.noreply.github.com>
2025-07-14 16:16:22 -0300

    Delete cafe/dist directory

--- cafe/dist/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const AuthController_1 = require("../controllers/AuthController");
-const router = (0, express_1.Router)();
-const authController = new AuthController_1.AuthController();
-router.post('/register', authController.register.bind(authController));
-router.post('/login', authController.login.bind(authController));
-exports.default = router;
